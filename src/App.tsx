99d84c90568e1b2b21b71569a5643097e21a1f4d
alirezafarmani <150049890+AlirezaFarmani1@users.noreply.github.com>
2024-09-29 23:04:31 +0330

    set routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Home from "./pages/home/Home";
+import HowToStart from "./pages/how-to-start/HowToStart";
+import OurService from "./pages/our-service/OurService";
+import Technologies from "./pages/technologies/Technologies";
+import Careers from "./pages/careers/Careers";
+import Contact from "./pages/contact/Contact";
+
 export default function App() {
   return (
     <>
-      <div>
-        <h2></h2>
-      </div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/how-to-start" element={<HowToStart />} />
+          <Route path="/our-service" element={<OurService />} />
+          <Route path="/technologies" element={<Technologies />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
     </>
   );
 }
